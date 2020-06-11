efae21e5385d82c52cbe3cf63429892f58663aea
ChenXiaojun <helloxiaojun@outlook.com>
2020-06-11 22:03:54 +0800

    添加了显示所有主题的页面 Topics.html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     # 主页
     path( '', views.index, name = 'index' ),
+
+    # 显示所有的主题
+    path( r'topics/',views.topics, name='topics' )
 ]
 app_name = 'learning_logs'
